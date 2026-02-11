139132fae55ba134aeedcdf8e896d65991676172
STEM-girlie <nasreenbaker.creative@gmail.com>
2026-02-11 19:51:12 +0000

    additional function

---
+++ GrasshopperMessiGoals.js
@@ -0,0 +1,9 @@
+let laLigaGoals = 5;
+let copaDelReyGoals = 10;
+let championsLeagueGoals = 2;
+
+function goals(laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
+  return laLigaGoals + copaDelReyGoals + championsLeagueGoals;
+}
+
+goals();
